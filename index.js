ce140092a0b1e8c0e52802fcac95c38c30ea29b7
tauri <tauri.nikkari@elion.ee>
2015-08-29 03:03:00 +0300

    Separate compiler code into module and make it asynchronous.

--- index.js
+++ index.js
@@ -1,71 +1,28 @@
 /**
- * input (failMidaParsida, KustOtsidaKomponente, JSONdata?, callback)
+ * input (listOfViews/dir?, componentDir,)
  * @type {*|exports|module.exports}
  */
-
-var cheerio =   require("cheerio");
+var compiler = require("./lib/compiler.js");
 var fs =        require('fs');
-var hogan =     require('hogan.js');
-var components;
 var componentsDir = "components";
-
 var htmlString = fs.readFileSync('views/test.html').toString();
-var $ = cheerio.load(htmlString);
+//var $ = cheerio.load(htmlString);
+
 //collect components
 fs.readdir(componentsDir,function(err,dir){
     if(err){
         throw err;
         return console.error(err);
     }
-    components = dir;
-    compileDom();
-});
-
-//compiles custom tags into predefined component html
-function compileDom(){
-    // search components
-    for (var i = 0; i < components.length; i++){
-        var componentName = components[i];
-        var $component = $(componentName);
-
-        //1 component may be more than once
-        for(var c = 0; c < $component.length; c++){
-           // console.log($component[c].tagName)
-            var cmpAtr = $($component[c]).attr();
-            var cmpCont = $($component[c]).html();
-            var templeate  = compileComponent(
-                    fs.readFileSync('components/'+componentName+'/templeate.html').toString(),
-                    $($component[c]).attr()
-                );
-            console.log(componentName,":",cmpCont);
-            if(cmpCont != "undefined"){
-                debugger;
-                var $c =  cheerio.load(templeate);
-                $c('content').replaceWith(
-                    cmpCont
-                )
+    //components = dir;
+    //console.log(dir)
+    compiler(htmlString,dir,function(err,data){
+        fs.writeFile("build/test.html", data, function(err) {
+            if(err) {
+                return console.log(err);
             }
-            $($component[c]).replaceWith(
-                $c.html()
-            );
-        }
-    }
-    //console.log($.html())
-    fs.writeFile("build/test.html", $.html(), function(err) {
-        if(err) {
-            return console.log(err);
-        }
 
-        console.log("The file was saved!");
-    });
-}
-
-//compiles mustache süntax inside component templeate
-function compileComponent(templeate,data){
-    return hogan.compile(templeate).render(data);
-}
-
-//content insert
-function insertConten(){
-    var $c = cheerio.load(htmlString);
-}
+            console.log("The file was saved!");
+        });
+    })
+});
