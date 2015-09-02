0199288894a2178430b56a712f121e65b5cf1099
tauri <tauri.nikkari@elion.ee>
2015-09-02 10:37:13 +0300

    Setting up Grunt

---
+++ gruntfile.js
@@ -0,0 +1,11 @@
+module.exports = function(grunt){
+    // load all configuration files
+    var configuration = require("require-grunt-configs")(grunt, "grunt/conf");
+    grunt.initConfig(configuration);
+
+    // load custom tasks
+    grunt.loadTasks("grunt/task");
+
+    // load installed npm tasks
+    require("load-grunt-tasks")(grunt);
+};
