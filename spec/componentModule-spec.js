d6c8cfbf6cea1732b9703151c1b25cbd2fe0176e
tauri <tauri.nikkari@elion.ee>
2015-09-22 09:33:04 +0300

    #4 Separate component compiler (1)

---
+++ spec/componentModule-spec.js
@@ -0,0 +1,17 @@
+var Component = require("../lib/component-module");
+var fs =       require('fs');
+describe("Compiled component",function(){
+   var data = {"foo":"dataFoo","bar":"dataBar"};
+   var template =  fs.readFileSync('test/components/test-component/template.html').toString();
+   var content = '<h3 class="test">TERE TAURI</h3>';
+   var comp = new Component(template,data,content);
+   var compiled = comp.compile();
+
+   it("does not contain mustache tags",function(){
+      expect(compiled).not.toMatch(/({+\s*[#/^>]?\s*)\w+.(}+)+/);
+   });
+
+    it("includes innerHTML content",function(){
+       expect(compiled).toEqual(jasmine.stringMatching(content));
+    });
+});
